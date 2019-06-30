a070a23e0aa81d2e95119fca6769248c9f6c5505
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-30 16:43:48 +0530

    fixed import SharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthComponent } from './auth.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
+    SharedModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: AuthComponent }])
   ]
